1d3c3fe039e29d5b52f96974d979c0922e05322f
masudrwd <info.masud21@gmail.com>
2024-11-25 14:53:08 +0600

    fix sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,23 +3,23 @@ import type { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
    return [
       {
-         url: process.env.BASE_URL || 'http://localhost:3000',
+         url: process.env.BASE_URL || '',
          lastModified: new Date(),
       },
       {
-         url: `${process.env.BASE_URL}/about` || 'http://localhost:3000',
+         url: `${process.env.BASE_URL}/about`,
          lastModified: new Date(),
       },
       {
-         url: `${process.env.BASE_URL}/contact` || 'http://localhost:3000',
+         url: `${process.env.BASE_URL}/contact`,
          lastModified: new Date(),
       },
       {
-         url: `${process.env.BASE_URL}/blog` || 'http://localhost:3000',
+         url: `${process.env.BASE_URL}/blog`,
          lastModified: new Date(),
       },
       {
-         url: `${process.env.BASE_URL}/services` || 'http://localhost:3000',
+         url: `${process.env.BASE_URL}/services`,
          lastModified: new Date(),
       },
    ];
